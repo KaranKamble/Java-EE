22d7749c0cb66c324cb4f8408809314be64fcbae
Karan Kamble <karanbk9375@gmail.com>
2024-12-04 09:40:46 +0530

    commit

--- BankServices/src/main/java/com/example/demo/Account.java
+++ BankServices/src/main/java/com/example/demo/Account.java
@@ -10,55 +10,71 @@ public class Account {
 	private String email;
 	private String dob;
 	private String gender;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public int getMobileNo() {
 		return mobileNo;
 	}
+
 	public void setMobileNo(int mobileNo) {
 		this.mobileNo = mobileNo;
 	}
+
 	public String getAadhar() {
 		return aadhar;
 	}
+
 	public void setAadhar(String aadhar) {
 		this.aadhar = aadhar;
 	}
+
 	public String getPan() {
 		return pan;
 	}
+
 	public void setPan(String pan) {
 		this.pan = pan;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getDob() {
 		return dob;
 	}
-	public void setDob( String dob) {
+
+	public void setDob(String dob) {
 		this.dob = dob;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	@Override
 	public String toString() {
 		return "BankService [name=" + name + ", address=" + address + ", mobileNo=" + mobileNo + ", aadhar=" + aadhar
